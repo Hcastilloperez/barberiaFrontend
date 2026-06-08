8648d0a55817fa5ddc696fd7813c292d7204ef4b
Hector Rafael Castillo Perez <hcastilloperez1979@gmail.com>
2026-06-08 00:30:04 -0500

    update appRoutes 3

--- src/appRoutes.jsx
+++ src/appRoutes.jsx
@@ -24,7 +24,7 @@ import PagosPage from "./Pages/Private/pagos/pagos";
 import BarberosPage from "@/Pages/Private/barberos/barberos";
 import InfoBarberoPage from "@/Pages/Private/barberos/InfoBarbero";
 
-import InfoOrdenesPage from "@/pages/Private/orders/infoOrdenes";
+import InfoOrdenesPage from "@/Pages/Private/orders/infoOrdenes";
 import DetailOrdenesPage from "@/Pages/Private/orders/DetailOrdenes";
 import HistoricoOrdenesPage from "@/Pages/Private/orders/HistoricoOrdenes";
 
